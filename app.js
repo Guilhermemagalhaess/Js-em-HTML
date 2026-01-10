450064151b88bbb7b3e3c35a8f295c8f90343473
Guilhermemagalhaess''git config --global user.email <gui.metal21@gmail.com>
2026-01-10 20:55:46 -0300

    Funções com parâmetros 2

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
 function exibirTextoNaTela(tag, texto)   {
    let campo = document.querySelector (tag);
-campo.innerHATML = texto;
+campo.innerHTML = texto;
  
 }
 
-exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoDaTela ('p', 'Escolha um número entre 1 e 10');
+exibirTextoNaTela ('h1', 'Jogo do número secreto');
+exibirTextoNaTela ('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute() {
     console.log ('O botão foi clicado');
